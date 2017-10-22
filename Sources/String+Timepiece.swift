dd6359871fcc9f2e693c082d7c1124ad5f784e8c
zhangdongpo <zdp930128@gmail.com>
2017-10-22 22:49:32 +0800

    ss

--- Sources/String+Timepiece.swift
+++ Sources/String+Timepiece.swift
@@ -17,9 +17,6 @@ extension String {
     public func date(inFormat format: String) -> Date? {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-
         return dateFormatter.date(from: self)
     }
 
